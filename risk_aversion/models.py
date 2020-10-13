eef30b7025b54377b1412477a1f7c664ae34b47e
evgenyvasilets <j.vas@mail.ru>
2020-10-13 15:08:37 +0200

    add prolific id to dodels

--- risk_aversion/models.py
+++ risk_aversion/models.py
@@ -220,6 +220,7 @@ class Player(BasePlayer):
         self.fixation_time_loss = randomized.loc[row_number, 'fixation_time_loss']
         self.fixation_time_gain = randomized.loc[row_number, 'fixation_time_gain']
         self.condition_number = randomized.loc[row_number, 'condition_number']
+        self.PROLIFIC_PID = self.participant.label
 
         return randomized
     def practice_trials(self):
